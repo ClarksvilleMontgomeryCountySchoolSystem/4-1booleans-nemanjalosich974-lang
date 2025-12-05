3ff12c2d79a2938ef45e66c5d967d9f4769941a9
nemanjalosich974-lang <nemanja.losich974@students.cmcss.net>
2025-12-05 10:22:01 -0600

    Update task.py

--- task.py
+++ task.py
@@ -2,10 +2,10 @@
 #--------Group 1----------
 # Question: "Are you wearing shoes right now?"
 
-is_wearing_shoes = True   # You are not wearing shoes.
+is_wearing_shoes = False   # You are not wearing shoes.
 
 # Question: "Did you eat breakfast today?"
-ate_breakfast_1 = False    # You ate breakfast this morning.
+ate_breakfast_1 = True    # You ate breakfast this morning.
 ate_breakfast_2 = False   # You did not eat breakfast this morning.
 
 # Question: "Are you feeling awake?"
@@ -18,12 +18,12 @@ has_phone = True     # You have a phone with you.
 
 # Question: "Are you sitting near the sink?"
 
-near_sink = False    # You are not sitting near the sink.
+near_sink = True    # You are not sitting near the sink.
 
 # ------Group 2--------
 
 # Question: "Are you NOT wearing a hat?"
-not_wearing_hat = True     # You have a hat on your head.
+not_wearing_hat = False     # You have a hat on your head.
 
 
 # Question: "Did you NOT forget your homework?"
@@ -45,5 +45,5 @@ not_in_front = False     # " Yes and the fridge too."
 
 # REFLECTION: Discuss with your partner
 # Which variable names were easiest to work with? Why?
-'''The variable names that were easiest to work with were the names
-# Which were most confusing? Why?
+'''The variable names that were easiest to work with were the names that did not have the word 'Not' in it. becasue the questions were straightforward and had nothing else there.
+# Which were most confusing? Why? 
